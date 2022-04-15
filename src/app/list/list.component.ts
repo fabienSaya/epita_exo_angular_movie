f8a02b9cd6f4fd866fe01dece81e1b16e53a0faf
fabienSAYA <fsayatravail@hotmail.com>
2022-04-15 17:40:42 +0200

    affichage des cards sans avoir encore créé de component

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -11,12 +11,19 @@ export class ListComponent implements OnInit {
 
   movies:Array<any> =[];
   url:string ="https://api.themoviedb.org/3/discover/movie?api_key=d8cc8cac11048db08e0fdd11acbf66c1&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate";
+  url_base_image:string ="https://image.tmdb.org/t/p/w500";
 
   /* injection d'un objet http de la classe HttpClient */
   constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
-    this.http.get(this.url).subscribe(response=> console.log(response));
+    /* requete pour récupérer les movies*/
+    this.http.get(this.url).subscribe((response:any)=> this.movies=response.results);
+
+  }
+
+  getImageUrl(backdrop_path:string) {
+    return this.url_base_image+backdrop_path;
   }
 
 }
